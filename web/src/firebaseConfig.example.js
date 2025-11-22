61c6d2716eda4c887bf9c2a7eb5d9c88aa3a65f3
Mirushamiru1 <asiimweshamiru573@gmail.com>
2025-11-22 07:44:33 -1100

    Update Firebase configuration with project details

--- web/src/firebaseConfig.example.js
+++ web/src/firebaseConfig.example.js
@@ -1,11 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Copy to firebaseConfig.js and fill with your Firebase web app config.
-export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: ""
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBQ0ct3dp-6ELEu0ImJmPl_xFh_W9nOezY",
+  authDomain: "mental-app-52732.firebaseapp.com",
+  projectId: "mental-app-52732",
+  storageBucket: "mental-app-52732.firebasestorage.app",
+  messagingSenderId: "851967923669",
+  appId: "1:851967923669:web:d706f6624f1689fa4dd95c",
+  measurementId: "G-2SXB3H3845"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
